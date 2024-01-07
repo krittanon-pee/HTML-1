5b7acc507ba1f9f81c27dce513a195b37ea7c459
krittanon-pee <151844482+krittanon-pee@users.noreply.github.com>
2024-01-07 09:54:04 +0700

    javascript

---
+++ main.js
@@ -0,0 +1,39 @@
+// String , number , boolean , array , object
+// 1) String
+let firstname = "Krittanon";
+const idcard = "B5814921";
+console.log("My name is",firstname);
+
+// 2) Number
+let number = 20;
+let height = 170.5;
+
+// 3) Boolean
+let isHandsome = true;
+
+firstname = "Tom";
+console.log("My name is",firstname);
+console.log("Number is",number);
+
+/*
++ บวก
+- ลบ
+* คูณ
+/ หาร
+% หารเอาเศษ
+*/
+
+let num1 = 10;
+let num2 = 5;
+let result = num1 % num2;
+console.log("Result is",result);
+
+// condition
+/*
+== เท่ากับ
+!= ไม่เท่ากับ
+> มากกว่า
+>= มากกว่าหรือเท่ากับ
+< น้อยกว่า
+<= น้อยกว่าหรือเท่ากับ
+*/
