32b1e333cf0d76d07feb3bb70a164d978a76d7ae
krittanon-pee <151844482+krittanon-pee@users.noreply.github.com>
2024-01-16 11:24:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -236,3 +236,47 @@ function calculate_grade(parameter){
 // การเรียกใช้งาน function
 let grade02 = calculate_grade(score2)
 console.log("score2 = " + grade2)
+
+
+/*
+object function array
+*/
+
+let students2 = [
+    {
+      name: "John",
+      score: 90,
+      grade: 'A'
+    },{
+        name: "Yoshi",
+        score: 80,
+        grade: 'B'
+    },{
+        name: "Mario",
+        score: 70,
+        grade: 'C'
+    }
+]
+//หาว่าอยู่ตรงไหน
+let student = students.find((s) => {
+  if (s.name == 'Yoshi') {
+    return true
+  }
+})
+
+//คูณตัวเลข 2 เท่า
+let doublescore_student = students.map((s) =>{
+  s.score = s.score *2
+  return s
+})
+
+let highscore_student = students.filter((s) =>{
+ if(s.score >=80){
+    return true
+ }
+})
+
+//การเรียกใช้ฟังค์ชั่น
+console.log('students',student);
+console.log('doublescore_student',doublescore_student)
+console.log('highscore_student',highscore_student)
